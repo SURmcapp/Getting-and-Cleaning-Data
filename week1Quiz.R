5f117c54a2dbc7181bd0e745ccfab61710a37343
SURmcapp <mark.cappiello@gmail.com>
2022-10-28 04:34:46 -0400

    commit

--- week1Quiz.R
+++ week1Quiz.R
@@ -3,3 +3,6 @@ df <- read.csv(file = 'getdata_data_ss06hid.csv')
 hv <- aggregate(df$ST, by=list(df$ST,df$VAL), FUN=length)
 
 dat <- readxl::read_xlsx('~/GitHub/Getting-and-Cleaning-Data/getdata_data_DATA.gov_NGAP.xlsx', range = 'G18:O23')
+
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml" 
+
